9d6ed687d2278c935f37888f48dd38fe7ef1c294
satyasanatanbastia <satyasanatanbastia@gmail.com>
2024-03-10 02:08:33 +0530

    minor change

---
+++ NestedLoop.py
@@ -0,0 +1,90 @@
+#Print the following pattern.
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print("*",end=" ")
+#     print()
+
+
+#Print the following pattern.
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# 1 2 3 4 5 
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print()
+
+
+
+#Print the following pattern.
+# 5 4 3 2 1 
+# 5 4 3 2 1 
+# 5 4 3 2 1 
+# 5 4 3 2 1 
+# 5 4 3 2 1 
+# for i in range(1,6):
+#     for j in range(5,0,-1):
+#         print(j,end=" ")
+#     print()
+
+
+
+#Print the following pattern.
+# 1 1 1 1 1 
+# 2 2 2 2 2 
+# 3 3 3 3 3 
+# 4 4 4 4 4 
+# 5 5 5 5 5 
+# for i in range(1,6):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+
+
+#Print the following pattern.
+# 5 5 5 5 5 
+# 4 4 4 4 4 
+# 3 3 3 3 3 
+# 2 2 2 2 2 
+# 1 1 1 1 1 
+# for i in range(5,0,-1):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+
+
+#Ask N from user. N means number of lines. Then print the following pattern.
+# 1 1 1 1 1 
+# 2 2 2 2 2 
+# 3 3 3 3 3 
+# 4 4 4 4 4 
+# n=int(input("Enter your No-"))
+# for i in range (1,n+1):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+
+
+# Ask N from user. N means number of lines. Then print the following pattern. 
+# 4 4 4 4 4 
+# 3 3 3 3 3 
+# 2 2 2 2 2 
+# 1 1 1 1 1
+# n=int(input("Enter your No-"))
+# for i in range (n,0,-1):
+#     for j in range(1,6):
+#         print(i,end=" ")
+#     print()
+
+
+
