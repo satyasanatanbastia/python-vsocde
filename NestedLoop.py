c5baea88e9a4164baf3917ee4f4c2067f5d015fd
satyasanatanbastia <satyasanatanbastia@gmail.com>
2024-03-11 01:51:46 +0530

    Minor Changes

--- NestedLoop.py
+++ NestedLoop.py
@@ -88,3 +88,150 @@
 
 
 
+#_______________________Advance Nested Loop Prtice___________________________
+
+
+
+# Print the following pattern.
+# 1 
+# 1 2 
+# 1 2 3 
+# 1 2 3 4 
+# 1 2 3 4 5 
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j ,end=" ")
+#     print()
+
+
+#. Print the following pattern.
+# 1 
+# 2 2 
+# 3 3 3 
+# 4 4 4 4 
+# 5 5 5 5 5 
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(i,end=" ")
+#     print()
+
+
+#Print the following pattern1 
+# 1
+# 2 1 
+# 3 2 1 
+# 4 3 2 1 
+# 5 4 3 2 1 
+
+# for i in range(1,6):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+
+#Print the following pattern
+# 5 
+# 5 4 
+# 5 4 3 
+# 5 4 3 2 
+# 5 4 3 2 1
+
+# for i in range(5,0,-1):
+#     for j in range(5,i-1,-1):
+#         print(j,end=" ")
+#     print()
+
+
+#Print the following pattern
+# 5 
+# 4 4 
+# 3 3 3 
+# 2 2 2 2 
+# 1 1 1 1 1 
+
+# for i in range(5,0,-1):
+#     for j in range(5,i-1,-1):
+#         print(i,end=" ")
+#     print()
+
+
+#Print the following pattern.
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print()
+
+
+#Print the following pattern.
+# 5 4 3 2 1 
+# 5 4 3 2 
+# 5 4 3 
+# 5 4 
+# 5 
+
+# for i in range(1,6):
+#     for j in range(5,i-1,-1):
+#         print(j,end=" ")
+#     print()
+
+
+#Print the following pattern.
+# 5 5 5 5 5 
+# 4 4 4 4 
+# 3 3 3 
+# 2 2 
+# 1 
+
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+
+
+#Print the following pattern.
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("*",end=" ")
+#     print()
+
+
+#Print the following pattern
+# 5 4 3 2 1 
+# 4 3 2 1 
+# 3 2 1 
+# 2 1 
+# 1 
+
+# for i in range(5,0,-1):
+#     for j in range(i,0,-1):
+#         print(j,end=" ")
+#     print()
+
+
+#Print the following pattern.
+# 1 
+# 2 3 
+# 4 5 6 
+# 7 8 9 10 
+# 11 12 13 14 15 
+
+# start = 1
+# for i in range(1, 6):
+#     for j in range(i):
+#         print(start, end=' ')
+#         start += 1
+#     print()
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,4 @@
-for name, score in student_scores.items():
-#     print(f"{name} scored {score}")
+for i in range(5,0,-1):
+    for j in range(i):
+        print(i,end=" ")
+    print()
