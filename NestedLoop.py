8c8c1b54320d458f8e9a3e95b8d69e8192b8a39a
satyasanatanbastia <satyasanatanbastia@gmail.com>
2024-03-11 20:50:31 +0530

    Minor Changes

--- NestedLoop.py
+++ NestedLoop.py
@@ -1,72 +1,68 @@
-#Print the following pattern.
-# * * * * * 
-# * * * * * 
-# * * * * * 
-# * * * * * 
-# * * * * * 
+# Print the following pattern.
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
+# * * * * *
 # for i in range(1,6):
 #     for j in range(1,6):
 #         print("*",end=" ")
 #     print()
 
 
-#Print the following pattern.
-# 1 2 3 4 5 
-# 1 2 3 4 5 
-# 1 2 3 4 5 
-# 1 2 3 4 5 
-# 1 2 3 4 5 
+# Print the following pattern.
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
+# 1 2 3 4 5
 # for i in range(1,6):
 #     for j in range(1,6):
 #         print(j,end=" ")
 #     print()
 
 
-
-#Print the following pattern.
-# 5 4 3 2 1 
-# 5 4 3 2 1 
-# 5 4 3 2 1 
-# 5 4 3 2 1 
-# 5 4 3 2 1 
+# Print the following pattern.
+# 5 4 3 2 1
+# 5 4 3 2 1
+# 5 4 3 2 1
+# 5 4 3 2 1
+# 5 4 3 2 1
 # for i in range(1,6):
 #     for j in range(5,0,-1):
 #         print(j,end=" ")
 #     print()
 
 
-
-#Print the following pattern.
-# 1 1 1 1 1 
-# 2 2 2 2 2 
-# 3 3 3 3 3 
-# 4 4 4 4 4 
-# 5 5 5 5 5 
+# Print the following pattern.
+# 1 1 1 1 1
+# 2 2 2 2 2
+# 3 3 3 3 3
+# 4 4 4 4 4
+# 5 5 5 5 5
 # for i in range(1,6):
 #     for j in range(1,6):
 #         print(i,end=" ")
 #     print()
 
 
-
-#Print the following pattern.
-# 5 5 5 5 5 
-# 4 4 4 4 4 
-# 3 3 3 3 3 
-# 2 2 2 2 2 
-# 1 1 1 1 1 
+# Print the following pattern.
+# 5 5 5 5 5
+# 4 4 4 4 4
+# 3 3 3 3 3
+# 2 2 2 2 2
+# 1 1 1 1 1
 # for i in range(5,0,-1):
 #     for j in range(1,6):
 #         print(i,end=" ")
 #     print()
 
 
-
-#Ask N from user. N means number of lines. Then print the following pattern.
-# 1 1 1 1 1 
-# 2 2 2 2 2 
-# 3 3 3 3 3 
-# 4 4 4 4 4 
+# Ask N from user. N means number of lines. Then print the following pattern.
+# 1 1 1 1 1
+# 2 2 2 2 2
+# 3 3 3 3 3
+# 4 4 4 4 4
 # n=int(input("Enter your No-"))
 # for i in range (1,n+1):
 #     for j in range(1,6):
@@ -74,11 +70,10 @@
 #     print()
 
 
-
-# Ask N from user. N means number of lines. Then print the following pattern. 
-# 4 4 4 4 4 
-# 3 3 3 3 3 
-# 2 2 2 2 2 
+# Ask N from user. N means number of lines. Then print the following pattern.
+# 4 4 4 4 4
+# 3 3 3 3 3
+# 2 2 2 2 2
 # 1 1 1 1 1
 # n=int(input("Enter your No-"))
 # for i in range (n,0,-1):
@@ -87,29 +82,27 @@
 #     print()
 
 
-
-#_______________________Advance Nested Loop Prtice___________________________
-
+# _______________________Advance Nested Loop Prtice___________________________
 
 
 # Print the following pattern.
-# 1 
-# 1 2 
-# 1 2 3 
-# 1 2 3 4 
-# 1 2 3 4 5 
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
 # for i in range(1,6):
 #     for j in range(1,i+1):
 #         print(j ,end=" ")
 #     print()
 
 
-#. Print the following pattern.
-# 1 
-# 2 2 
-# 3 3 3 
-# 4 4 4 4 
-# 5 5 5 5 5 
+# . Print the following pattern.
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
 
 # for i in range(1,6):
 #     for j in range(1,i+1):
@@ -117,12 +110,12 @@
 #     print()
 
 
-#Print the following pattern1 
+# Print the following pattern1
 # 1
-# 2 1 
-# 3 2 1 
-# 4 3 2 1 
-# 5 4 3 2 1 
+# 2 1
+# 3 2 1
+# 4 3 2 1
+# 5 4 3 2 1
 
 # for i in range(1,6):
 #     for j in range(i,0,-1):
@@ -130,11 +123,11 @@
 #     print()
 
 
-#Print the following pattern
-# 5 
-# 5 4 
-# 5 4 3 
-# 5 4 3 2 
+# Print the following pattern
+# 5
+# 5 4
+# 5 4 3
+# 5 4 3 2
 # 5 4 3 2 1
 
 # for i in range(5,0,-1):
@@ -143,12 +136,12 @@
 #     print()
 
 
-#Print the following pattern
-# 5 
-# 4 4 
-# 3 3 3 
-# 2 2 2 2 
-# 1 1 1 1 1 
+# Print the following pattern
+# 5
+# 4 4
+# 3 3 3
+# 2 2 2 2
+# 1 1 1 1 1
 
 # for i in range(5,0,-1):
 #     for j in range(5,i-1,-1):
@@ -156,12 +149,12 @@
 #     print()
 
 
-#Print the following pattern.
-# * 
-# * * 
-# * * * 
-# * * * * 
-# * * * * * 
+# Print the following pattern.
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
 
 # for i in range(1,6):
 #     for j in range(1,i+1):
@@ -169,12 +162,12 @@
 #     print()
 
 
-#Print the following pattern.
-# 5 4 3 2 1 
-# 5 4 3 2 
-# 5 4 3 
-# 5 4 
-# 5 
+# Print the following pattern.
+# 5 4 3 2 1
+# 5 4 3 2
+# 5 4 3
+# 5 4
+# 5
 
 # for i in range(1,6):
 #     for j in range(5,i-1,-1):
@@ -182,12 +175,12 @@
 #     print()
 
 
-#Print the following pattern.
-# 5 5 5 5 5 
-# 4 4 4 4 
-# 3 3 3 
-# 2 2 
-# 1 
+# Print the following pattern.
+# 5 5 5 5 5
+# 4 4 4 4
+# 3 3 3
+# 2 2
+# 1
 
 # for i in range(5,0,-1):
 #     for j in range(i):
@@ -195,12 +188,12 @@
 #     print()
 
 
-#Print the following pattern.
-# * * * * * 
-# * * * * 
-# * * * 
-# * * 
-# * 
+# Print the following pattern.
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
 
 # for i in range(5,0,-1):
 #     for j in range(i):
@@ -208,12 +201,12 @@
 #     print()
 
 
-#Print the following pattern
-# 5 4 3 2 1 
-# 4 3 2 1 
-# 3 2 1 
-# 2 1 
-# 1 
+# Print the following pattern
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
 
 # for i in range(5,0,-1):
 #     for j in range(i,0,-1):
@@ -221,12 +214,12 @@
 #     print()
 
 
-#Print the following pattern.
-# 1 
-# 2 3 
-# 4 5 6 
-# 7 8 9 10 
-# 11 12 13 14 15 
+# Print the following pattern.
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
 
 # start = 1
 # for i in range(1, 6):
@@ -234,4 +227,3 @@
 #         print(start, end=' ')
 #         start += 1
 #     print()
-
